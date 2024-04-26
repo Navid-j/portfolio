69e9880116c46a90b78cee70652853e5d1b03df8
Navid Jahangiri <n_doown@yahoo.com>
2024-04-26 17:22:00 +0000

    fix error

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import Image from 'next/image';
 import {FC, memo} from 'react';
 
-import {aboutData, SectionId} from '../../data/data';
+//import {aboutData, SectionId} from '../../data/data';
 import Section from '../Layout/Section';
 
 const About: FC = memo(() => {
