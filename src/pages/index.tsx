64cd5443ae93f890dd13d3dd46777f7496beafbf
Navid Jahangiri <n_doown@yahoo.com>
2024-04-26 18:16:36 +0000

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,10 @@ import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
-import About from '../components/Sections/About';
-import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
-import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
 
 // eslint-disable-next-line react-memo/require-memo
@@ -20,11 +17,8 @@ const Home: FC = memo(() => {
     <Page description={description} title={title}>
       <Header />
       <Hero />
-      <About />
       <Resume />
       <Portfolio />
-      <Testimonials />
-      <Contact />
       <Footer />
     </Page>
   );
